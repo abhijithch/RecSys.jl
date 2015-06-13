4345d190c7bbb2dbed2a93b5ead6902fcea2ed14
domarps <spmd92@gmail.com>
2015-06-13 19:21:28 +0530

    Adding DataFrame Demo

--- examples/hdf5_demo/Test.jl
+++
@@ -1,7 +0,0 @@
-using HDF5
-cd("/RecSys.jl/examples/hdf5_demo/")
-file = h5open("test.h5", "w")
-g = g_create(file, "mygroup") # create a group
-g["dset1"] = 3.2              # create a scalar dataset
-attrs(g)["Description"] = "This group contains only a single dataset" # an attribute
-close(file)
