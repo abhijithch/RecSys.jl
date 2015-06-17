75a5e797f6542273d479ebe6c936d3291e1582a4
domarps <spmd92@gmail.com>
2015-06-17 11:23:07 +0530

    Added functionality for Jester Data

--- src/io.jl
+++ src/io.jl
@@ -51,11 +51,20 @@ function read_jester(file)
   # http://www.ieor.berkeley.edu/%7Egoldberg/jester-data/
   # /home/pramod/Desktop/RecSysData/Various DataSets/jester-data-1.xls
   f  = open(file);
-  df     = DataFrame(userId,itemId,rating)
-  tempDf = Taro.readxl(file, "Sheet1", "A1:CW24983"; header=false)
-  
-  for 
-  
+  dfJesterTemp = Taro.readxl("/home/pramod/Desktop/RecSysData/Various_DataSets/jester-data-1.xls",
+                         "jester-data-1-new", 
+			 "A1:CW24983"; 
+			  header=false)
+  delete!(dfJesterTemp, :x2)			  
+  [dfJesterTemp[dfJesterTemp[nm] .== 99, nm] = -100 for nm in names(dfJesterTemp)]
+  (rows,cols) = size(dfJesterTemp)
+  JesterData  = DataFrame(userId = Int64[], itemID = Int64[] , rating = Float64[])
+  for r = 1:rows
+    for c = 1:cols
+         push!(JesterData,[r,c,dfJesterTemp[r,c]])
+    end
+  end
+
 end
 
 function read_lastfm(file)
