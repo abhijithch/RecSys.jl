a46c30be9b1f538471c62bc6c04e95b8aa14a18c
Abhijith Chandraprabhu <abhijithc@gmail.com>
2015-07-05 17:03:14 +0530

    nprocs 1 fix

---
+++ src/RecSys.jl
@@ -0,0 +1,11 @@
+module RecSys
+
+export ALSFactorization, loadData
+
+println("Welcome to ALSFactorization")
+include("LoadData.jl")
+include("Utilities.jl")
+include("EveryWhere.jl")
+include("ALSFactorization.jl")
+
+end
