a112d588fe9ed141e9813d7a0a0fa8191fde0edb
Raghu Nandan Chilukuri <raghu.rnc@gmail.com>
2015-09-29 17:57:52 +0530

    Add new user rating pages

--- examples/demo/new_page.jl
+++ examples/demo/new_page.jl
@@ -1,5 +1,6 @@
 using Reactive
-include("index.jl")
+using JSON
+include("ALS.jl")
 
 movie_meta = JSON.parse(readall("movie_info.json"))
 
@@ -8,14 +9,27 @@ getfield(m, x, def="") = get(movie_meta, m, Dict()) |>
 
 
 function showmovie(m)
-    poster = image(getfield(m, "http://uwatch.to/posters/placeholder.png"), alt=m) |>
+    poster = image(getfield(m,"Poster", "http://uwatch.to/posters/placeholder.png"), alt=m) |>
        size(120px, 180px)
     desc = vbox( fontsize(1.5em, m),
                getfield(m, "Genre")
                )
-    rating_radio = radiogroup(vec(radio([string("rate", i) for i=1:5]) ), 
-                              name="Rate this movie"
-                             )
+    #rating_radio = radiogroup([radio(:rate, "Rate $i") for i in 1:5] , 
+    #                          name="Rate this movie"
+    #                         )
+
+    #rate = string("Rate me!") |> radio(:rating; toggles=true, disabled=false) 
+    rate = slider(0:5; name="Your rating", value=0, editable=true, pin=true, disabled=false, secondaryprogress=0)
+                 
+    vbox(poster,vskip(1em), desc, vskip(1em), rate)
+
+end
+
+
+function getmovies(movie_set)
+    vbox(intersperse(vbox( vskip(1em), hline(), vskip(1em)), 
+                               map(showmovie, movie_set[floor(rand(10)*1000), 2]  )                
+                              ) )
 
 end
 
@@ -23,18 +37,30 @@ end
 
 function main(window)
     push!(window.assets, "widgets")
-    push!(window.assets, "layout")
+    push!(window.assets, "layout2")
 
-    vlist1 = vbox()
-    vlist2 = vbox()
-    vlist3 = vbox()
+    movie_numbers =  floor(rand(10)*1000)
+    movie_set = readdlm("movies.csv",'\,')
+    #movie_tile = map(showmovie, movie_set[movie_numbers, 2])
 
+    vlist0 = vbox(title(1, "To get recommendations, rate some movies first"))
 
-    hbox(vlist1, vlist2, vlist3)
+    vlist1 = vbox( intersperse(vbox( vskip(1em), hline(), vskip(1em)), 
+                               flex(map(showmovie, movie_set[floor(rand(10)*1000), 2]  ) ) 
+                              ) 
+                 )
+    vlist2 = vbox( intersperse(vbox( vskip(1em), hline(), vskip(1em)), 
+                               flex(map(showmovie, movie_set[floor(rand(10)*1000), 2]  )  )              
+                              )    
+                 )
+    vlist3 = vbox( intersperse(vbox( vskip(1em), hline(), vskip(1em)), 
+                               flex(map(showmovie, movie_set[floor(rand(10)*1000), 2]  ) )               
+                              )    
+                 )
 
-
-
-
+    #displayedlist = hbox( vlist0, intersperse(vskip(1em), hline(), vskip(1em)),   map(getmovies, [1:3]))
+   
+    display = vbox(vlist0, vskip(3em), hbox(flex(vlist1), flex(vlist2), flex(vlist3)) )
 
 
 end
