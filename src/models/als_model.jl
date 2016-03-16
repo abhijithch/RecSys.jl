70bf9c3ca0031f8fbe7a34f85a2f65941beb73f7
Abhijith Chandraprabhu <abhijithc@gmail.com>
2016-03-16 10:04:25 +0530

    Revert "chunks externalized as Blobs.jl"

--- src/models/als_model.jl
+++ src/models/als_model.jl
@@ -7,7 +7,7 @@ type SharedMemoryModel <: Model
     Pinv::Nullable{ModelFactor}
 end
 
-nusers(model::SharedMemoryModel) = size(model.U, 2)
+nusers(model::SharedMemoryModel) = size(model.U, 1)
 nitems(model::SharedMemoryModel) = size(model.P, 2)
 nfactors(model::SharedMemoryModel) = model.nfactors
 
@@ -30,6 +30,7 @@ function localize!(model::SharedMemoryModel)
     isa(model.P, SharedArray) && (model.P = copy(model.P))
     nothing
 end
+sync!(model::SharedMemoryModel) = nothing
 
 function clear(model::SharedMemoryModel)
     model.lambdaI = nothing
@@ -51,6 +52,8 @@ function pinv(model::SharedMemoryModel)
     end
     get(model.Pinv)
 end
+
+vec_mul_p(model::SharedMemoryModel, v) = v * model.P
 vec_mul_pinv(model::SharedMemoryModel, v) = v * pinv(model)
 
 function prep{TI<:Inputs}(inp::TI, nfacts::Int, lambda::Float64)
@@ -61,7 +64,7 @@ function prep{TI<:Inputs}(inp::TI, nfacts::Int, lambda::Float64)
     nu = nusers(inp)
     ni = nitems(inp)
 
-    U = zeros(nfacts, nu)
+    U = zeros(nu, nfacts)
     P = rand(nfacts, ni)
     for idx in 1:ni
         P[1,idx] = mean(all_user_ratings(inp, idx))
@@ -74,3 +77,16 @@ function prep{TI<:Inputs}(inp::TI, nfacts::Int, lambda::Float64)
     @logmsg("prep time: $(t2-t1)")
     model
 end
+
+@inline function setU(model::SharedMemoryModel, u::Int64, vals)
+    model.U[u,:] = vals
+    nothing
+end
+
+@inline function setP(model::SharedMemoryModel, i::Int64, vals)
+    model.P[:,i] = vals
+    nothing
+end
+
+getU(model::SharedMemoryModel, users) = model.U[users, :]
+getP(model::SharedMemoryModel, items) = model.P[:, items]
