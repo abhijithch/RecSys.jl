9b183b90c8ddf296eb516ff160a51e4c6799b432
tan <tanmaykm@gmail.com>
2016-04-15 11:56:30 +0530

    netflix example with blobs

--- examples/netflix/netflix.jl
+++ examples/netflix/netflix.jl
@@ -13,3 +13,18 @@ function test(matfile, entryname)
     println("Recommended: $recommended")
     nothing
 end
+
+function test_chunks(dataset_path, model_path)
+    mem = Base.Sys.free_memory()
+    mem_model = mem_inputs = round(Int, mem/3)
+    user_item_ratings = SparseBlobs(joinpath(dataset_path, "splits", "R_itemwise"); maxcache=mem_model)
+    item_user_ratings = SparseBlobs(joinpath(dataset_path, "splits", "RT_userwise"); maxcache=mem_model)
+
+    rec = ALSWR(user_item_ratings, item_user_ratings, ParBlob())
+
+    train(rec, 10, 4, model_path, mem_inputs)
+
+    err = rmse(rec)
+    println("rmse of the model: $err")
+    nothing
+end
